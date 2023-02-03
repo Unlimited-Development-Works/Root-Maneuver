d3d845b410398f5734ee57870bb125ee0846cf63
Daniel Atkinson <hourd.tasa@gmail.com>
2023-02-03 23:51:10 +0000

    Add background audio track to DanScene

--- Assets/Scripts/AudioController.cs
+++ Assets/Scripts/AudioController.cs
@@ -4,15 +4,9 @@ using UnityEngine;
 
 public class AudioController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public AudioSource BackgroundTrack;
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        BackgroundTrack.Play();
     }
 }
