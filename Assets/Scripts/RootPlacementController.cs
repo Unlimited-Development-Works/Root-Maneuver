1e191dfc412789eb95f70fce684bd65ce4a8db9c
Daniel Atkinson <hourd.tasa@gmail.com>
2023-02-04 12:37:29 +0000

    initial controller creation for placing root pieces

---
+++ Assets/Scripts/RootPlacementController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RootPlacementController : MonoBehaviour
+{
+    public GameObject rootPiece;
+    public GameObject rootNutrientPiece;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
