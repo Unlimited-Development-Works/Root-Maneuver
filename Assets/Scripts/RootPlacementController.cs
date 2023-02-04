816d7d9afd5aa969284c18967c271fc447cea7d1
Daniel Atkinson <hourd.tasa@gmail.com>
2023-02-04 13:00:21 +0000

    Place root peices whent he player gets far enough away from the previous piece

--- Assets/Scripts/RootPlacementController.cs
+++ Assets/Scripts/RootPlacementController.cs
@@ -4,16 +4,37 @@ using UnityEngine;
 
 public class RootPlacementController : MonoBehaviour
 {
+    public Rigidbody2D player;
     public GameObject rootPiece;
     public GameObject rootNutrientPiece;
+    private Transform lastLocation;
+    private float distance = 0;
+    public float spawnDistance;
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        distance = DistanceBetweenTwoTransforms(player.transform, lastLocation);
+        if (distance > spawnDistance)
+        {
+            GameObject spawnedRoot = Instantiate(rootPiece, player.transform.position, player.transform.rotation);
+            lastLocation = spawnedRoot.transform;
+        }
+    }
+
+    public void SetLastLocation(Transform location)
+    {
+        lastLocation = location;
+        Debug.Log("Set lastLocation to");
+        Debug.Log(lastLocation);
+    }
+
+    float DistanceBetweenTwoTransforms(Transform source, Transform target)
+    {
+        Vector2 delta = source.position - target.position;
+        return delta.magnitude;
     }
 }
