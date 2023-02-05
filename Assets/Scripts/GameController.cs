1002d4679f1c2c857ac87229439a163af2deb41e
Daniel Atkinson <hourd.tasa@gmail.com>
2023-02-05 11:00:17 +0000

    Set up border colliders in code

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -23,6 +23,7 @@ public class GameController : MonoBehaviour
         scores["Player_1"] = 0;
         scores["Player_2"] = 0;
         winFrame.SetActive(false);
+        PutColliders();
     }
 
     void Update()
@@ -74,5 +75,53 @@ public class GameController : MonoBehaviour
     {
         //SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         SceneManager.LoadScene("TitleScene");
+    }
+
+    private void PutColliders()
+    {
+        GameObject left = new GameObject("LeftCollider", typeof(BoxCollider2D), typeof(Rigidbody2D));
+        left.layer = 6;
+        left.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
+        GameObject right = new GameObject("RightCollider", typeof(BoxCollider2D));
+        right.layer = 6;
+        right.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
+        //GameObject top = new GameObject("TopCollider", typeof(BoxCollider2D));
+        GameObject bottom = new GameObject("BottomCollider", typeof(BoxCollider2D));
+        bottom.layer = 6;
+        bottom.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
+        Camera mainCamera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+
+        left.transform.SetParent(transform);
+        right.transform.SetParent(transform);
+        //top.transform.SetParent(transform);
+        bottom.transform.SetParent(transform);
+
+        Vector2 leftBottomCorner = mainCamera.ViewportToWorldPoint(Vector3.zero);
+        Vector2 rightTopCorner = mainCamera.ViewportToWorldPoint(Vector3.one);
+
+        Debug.LogWarning(leftBottomCorner);
+        Debug.Log(rightTopCorner);
+
+        left.transform.position = new Vector2(
+            leftBottomCorner.x - 0.5f,
+            mainCamera.transform.position.y
+            );
+        right.transform.position = new Vector2(
+            rightTopCorner.x + 0.5f,
+            mainCamera.transform.position.y
+            );
+/*        top.transform.position = new Vector2(
+            mainCamera.transform.position.x,
+            rightTopCorner.y + 0.5f
+            );*/
+        bottom.transform.position = new Vector2(
+            mainCamera.transform.position.x,
+            leftBottomCorner.y - 0.5f
+            );
+
+        left.transform.localScale = new Vector3(1, Mathf.Abs(rightTopCorner.y - leftBottomCorner.y));
+        right.transform.localScale = new Vector3(1, Mathf.Abs(rightTopCorner.y - leftBottomCorner.y));
+        //top.transform.localScale = new Vector3(Mathf.Abs(rightTopCorner.x - leftBottomCorner.x), 1);
+        bottom.transform.localScale = new Vector3(Mathf.Abs(rightTopCorner.x - leftBottomCorner.x), 1);
     }
 }
