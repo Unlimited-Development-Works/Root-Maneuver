695743761cb92288df8620ac6931a24cd12f4045
fierydrake <m.j.tunnicliffe@gmail.com>
2023-02-05 01:01:43 +0000

    Plant smoother scaling

--- Assets/Scripts/PlantController.cs
+++ Assets/Scripts/PlantController.cs
@@ -4,18 +4,36 @@ using UnityEngine;
 
 public class PlantController : MonoBehaviour
 {
-    public int currentGrowth = 0;
     public SpriteRenderer plantSpriteRenderer;
     public List<Sprite> plantSprites;
+    public List<float> plantScales;
     public int nutrientMultiplier = 5;
+    public float scaleBoost = 3f;
+
+    public int targetGrowth = 0;
+    private int currentGrowth = 0;
+    private Cooldown growthCooldown = new Cooldown(0.1f);
+
     public void AddGrowth(int nutrients)
     {
-        currentGrowth += nutrients;
-        int plantSpriteID = (currentGrowth / nutrientMultiplier);
-        if (plantSpriteID >= plantSprites.Count)
-        {
-            plantSpriteID = plantSprites.Count - 1;
+        targetGrowth += nutrients;
+    }
+
+    void Update() {
+        growthCooldown.Update();
+        if (targetGrowth > currentGrowth && growthCooldown.Expired()) {
+            currentGrowth += 1;
+            growthCooldown.Reset();
+            
+            int plantSpriteID = (currentGrowth / nutrientMultiplier);
+            if (plantSpriteID >= plantSprites.Count)
+            {
+                plantSpriteID = plantSprites.Count - 1;
+            }
+            plantSpriteRenderer.enabled = (currentGrowth > 0);
+            plantSpriteRenderer.sprite = plantSprites[plantSpriteID];
+            float scale = (float)(currentGrowth + 1) / (float)(plantSprites.Count * nutrientMultiplier);
+            transform.localScale = scale * plantScales[plantSpriteID] * scaleBoost * Vector3.one;
         }
-        plantSpriteRenderer.sprite = plantSprites[plantSpriteID];
     }
 }
